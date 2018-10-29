88e3878e4e181e246316a9db5fccad09a71f9dc6
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-10-29 13:55:11 -0400

    Added missing core_functions scripts

---
+++ analysis_scripts/chimera-wt/core_functions.R
@@ -0,0 +1,32 @@
+# get in the embryo functions
+
+source("/nfs/research1/marioni/jonny/embryos/scripts/core_functions.R")
+
+
+load_data = function(normalise = TRUE){
+  require(scran)
+  require(scater)
+  require(SingleCellExperiment)
+  require(Matrix)
+  
+  # counts = readMM("/nfs/research1/marioni/jonny/chimera-wt/data/raw_counts.mtx")
+  counts = readRDS("/nfs/research1/marioni/jonny/chimera-wt/data/raw_counts.rds")
+  genes <<- read.table("/nfs/research1/marioni/jonny/chimera-wt/data/genes.tsv", stringsAsFactors = F)
+  meta <<- read.table("/nfs/research1/marioni/jonny/chimera-wt/data/meta.tab", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  
+  
+  rownames(counts) = genes[,1] #ensembl
+  colnames(counts) = meta$cell
+
+  sce <<- SingleCellExperiment(assays = list("counts" = as(counts, "dgCMatrix")))
+  
+  if(normalise){
+    sfs <<- read.table("/nfs/research1/marioni/jonny/chimera-wt/data/sizefactors.tab", stringsAsFactors = F)[,1]
+    sizeFactors(sce) = sfs
+    sce <<- normalize(sce)
+  }
+  
+  invisible(0)
+}
+
+
