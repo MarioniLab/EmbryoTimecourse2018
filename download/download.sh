0bd8e272ead2a7b869ec14e8b9a2aa931351899b
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-12-06 10:11:40 +0000

    Added arrayexpress links in comments

--- download/download.sh
+++ download/download.sh
@@ -13,7 +13,13 @@ confirm() {
     esac
 }
 
+#below each line is the arrayexpress link
+#download seems slower, but probably the server is more reliable.
 confirm "Download Atlas data? [y/N]" && curl https://content.cruk.cam.ac.uk/jmlab/atlas_data.tar.gz > atlas_data.tar.gz
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6967/E-MTAB-6967.processed.1.zip
 confirm "Download WT-chimera data? [y/N]" && curl https://content.cruk.cam.ac.uk/jmlab/chimera_wt_data.tar.gz > chimera_wt_data.tar.gz
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7324/E-MTAB-7324.processed.1.zip
 confirm "Download Tal1-chimera data? [y/N]" && curl https://content.cruk.cam.ac.uk/jmlab/chimera_tal1_data.tar.gz > chimera_tal1_data.tar.gz
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-7325/E-MTAB-7325.processed.1.zip
 confirm "Download singularity images? [y/N]" && curl https://content.cruk.cam.ac.uk/jmlab/singularity.tar.gz > singularity.tar.gz
+# no AE equivalent
